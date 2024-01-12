ba9809a6e6e715c2bcbfb8e30c4c10796dc11120
ROHITGORULE <rohitgorule05@gmail.com>
2024-01-12 20:28:48 +0530

    login

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText usernameEditText=(EditText) findViewById(R.id.passwordEditText);
+        EditText passwordEditText=(EditText) findViewById(R.id.passwordEditText);
+        Button bb=(Button) findViewById(R.id.submitButton);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usernameEditText.getText().toString().equals("Rohit")&(passwordEditText.getText().toString().equals("727646")))
+                {
+                    Toast.makeText(MainActivity.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
